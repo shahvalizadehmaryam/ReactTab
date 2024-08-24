80c81de4048e2fd35a3b0787505507be719eff21
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-08-24 11:50:08 +0330

    set dommy data to tab comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css"
+import Tab from "./components/Tab/Tab";
 
 function App() {
 
-
+  const tabs = [
+    { label: 'Tab 1', content: 'This is the content of Tab 1' },
+    { label: 'Tab 2', content: 'This is the content of Tab 2' },
+    { label: 'Tab 3', content: 'This is the content of Tab 3' },
+  ];
   return (
     <div>
-
+     <Tab tabs={tabs} />
     </div>
   )
 }
---
+++ src/components/Tab/Tab.jsx
@@ -0,0 +1,9 @@
+const Tab = ({tabs}) => {
+    return ( 
+        <div>
+            <h3>tab</h3>
+        </div>
+     );
+}
+ 
+export default Tab;
