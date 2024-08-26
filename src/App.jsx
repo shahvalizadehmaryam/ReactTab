9838cde2675705b1cb92fe89c67b9b02d83d3a8a
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-08-26 09:07:03 +0330

    i added tab component and related style to it.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import "./App.css"
 import Tab from "./components/Tab/Tab";
 
 function App() {
-
   const tabs = [
-    { label: 'Tab 1', content: 'This is the content of Tab 1' },
-    { label: 'Tab 2', content: 'This is the content of Tab 2' },
-    { label: 'Tab 3', content: 'This is the content of Tab 3' },
+    {
+      label: "Tab 1",
+      heading: "Heading for Tab 1",
+      content: "This is the paragraph content of Tab 1.",
+    },
+    {
+      label: "Tab 2",
+      heading: "Heading for Tab 2",
+      content: "This is the paragraph content of Tab 2.",
+    },
+    {
+      label: "Tab 3",
+      heading: "Heading for Tab 3",
+      content: "This is the paragraph content of Tab 3.",
+    },
+    {
+      label: "Tab 4",
+      heading: "Heading for Tab 4",
+      content: "This is the paragraph content of Tab 4.",
+    },
   ];
+ 
   return (
     <div>
+      <h1>Tabs Component With React</h1>
      <Tab tabs={tabs} />
     </div>
   )
--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,9 +1,29 @@
-const Tab = ({tabs}) => {
-    return ( 
-        <div>
-            <h3>tab</h3>
+import { useState } from "react";
+import "./Tab.css"
+
+const Tab = ({ tabs }) => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  return (
+    <div>
+      <div className="tab-header-container">
+        <ul>
+          {tabs.map((tab,index) => (
+            <li key={index}>
+              <button className={`${activeTabIndex === index ? "active-btn-tab" : ""} tab-btn`} onClick={() => setActiveTabIndex(index)}>{tab.label}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="tab-content-container">
+      {tabs.map((tab,index) => (
+        <div key={index} className={ activeTabIndex === index ? "active" : "tab-content"}>
+          <h3>{tab.heading}</h3>
+          <p>{tab.content}</p>
         </div>
-     );
-}
- 
+      ))}
+      </div>
+    </div>
+  );
+};
+
 export default Tab;
